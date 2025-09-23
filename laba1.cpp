53b70330f400359a592508c90eed87ce7d6a8da8
kristina scherbakova <kristinagrom876@gmail.com>
2025-09-23 20:16:55 +0700

    изменила лабу

--- laba1.cpp
+++ laba1.cpp
@@ -1,3 +1,8 @@
+/*****************************
+ * Автор: Щербакова Кристина *
+ * Вариант: 14               *
+ *****************************/
+
 #include <iostream>
 #include <cmath>
 
@@ -5,25 +10,33 @@ using namespace std;
 
 int main() {
   const double pi = 3.1415926535;
-  double a        = 6.35;
-  double A_deg    = 16;
-  double B_deg    = 44;
+  double a, A_deg, B_deg, A, B, C;
+  double b, c, Beta_a, m_a, h_a;
   
-  double A = A_deg * pi / 180.0;
-  double B = B_deg * pi / 180.0;
-  double C = pi - A - B;
-  
+  cout << "a = ";
+  cin  >> a;
+    
+  cout << "A_deg = ";
+  cin  >> A_deg;
+    
+  cout << "B_deg = ";
+  cin  >> B_deg;
+ 
+  A = A_deg * pi / 180.0;
+  B = B_deg * pi / 180.0;
+  C = pi - A - B;
+
   double sina = sin(A);
   double sinB = sin(B);
   double sinc = sin(C);
-  
-  double b = (a * sinB / sina);
-  double c = (a * sinc / sina);
-  
-  double Beta_a = (2 * b * c * cos(A / 2)) / (b + c);
-  double m_a    = 0.5 * sqrt(2 * b * b + 2 * c * c - a * a);
-  double h_a    = b * sin(C);
-  
+
+  b = (a * sinB / sina);
+  c = (a * sinc / sina);
+
+  Beta_a = (2 * b * c * cos(A / 2)) / (b + c);
+  m_a    = 0.5 * sqrt(2 * b * b + 2 * c * c - a * a);
+  h_a    = b * sin(C);
+
   cout << "b      = " << b / 100 << endl;
   cout << "c      = " << c / 100 << endl;
   cout << "Beta_a = " << Beta_a / 100 << endl;
