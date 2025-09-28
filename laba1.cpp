4eab7b8ba3795330014d0fb60df9b61077312530
kristina scherbakova <kristinagrom876@gmail.com>
2025-09-28 22:00:05 +0700

    Внесла изменения

--- laba1.cpp
+++ laba1.cpp
@@ -26,22 +26,18 @@ int main() {
   B = B_deg * pi / 180.0;
   C = pi - A - B;
 
-  double sina = sin(A);
-  double sinB = sin(B);
-  double sinc = sin(C);
+  b = a * (sin(B) / sin(A));
+  c = a * (sin(C) / sin(A));
 
-  b = (a * sinB / sina);
-  c = (a * sinc / sina);
+  Beta_a = (2.0 * b * c * cos(A / 2.0)) / (b + c);
+  m_a    = 0.5 * sqrt(2.0 * b * b + 2.0 * c * c - a * a);
+  h_a    = c * sin(B);
 
-  Beta_a = (2 * b * c * cos(A / 2)) / (b + c);
-  m_a    = 0.5 * sqrt(2 * b * b + 2 * c * c - a * a);
-  h_a    = b * sin(C);
-
-  cout << "b      = " << b / 100 << endl;
-  cout << "c      = " << c / 100 << endl;
-  cout << "Beta_a = " << Beta_a / 100 << endl;
-  cout << "m_a    = " << m_a / 100 << endl;
-  cout << "h_a    = " << h_a / 100 << endl;
+  cout << "b      = " << b / 100.0      << endl;
+  cout << "c      = " << c / 100.0      << endl;
+  cout << "Beta_a = " << Beta_a / 100.0 << endl;
+  cout << "m_a    = " << m_a / 100.0    << endl;
+  cout << "h_a    = " << h_a / 100.0    << endl;
 
   return 0; 
 }
